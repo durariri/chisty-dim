d041fa6245c39fb33c1165c2f5ad41d6254f1563
Riana <131337305+durariri@users.noreply.github.com>
2023-11-28 08:37:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ const redirectServ = () => window.location.href = "./pages/service/index.html";
 
 const redirectAbout = () => window.location.href = "./pages/about-us/index.html";
 
+const redirectfind = () => window.location.href = "./pages/about-us/index.html";
+
+
